909ed26a25d34197e35d07dac13f83cc9e3bd21c
Michael <AstroMike101@gmail.com>
2021-08-02 15:53:39 -0700

    added JS, somewhat working for single digits

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+let screen = document.querySelector('.display');
+let deleteButton = document.querySelector('.deleteButton');
+let clearButton= document.querySelector('.clearButton');
+let click = document.querySelector('.click'); // sound of click on buttons
+let equation = 0;
+
+
+
+
+
 //plays a sound whenever a button is clicked
-const buttons = document.querySelectorAll('.btn')
+const buttons = document.querySelectorAll('.btn');
 buttons.forEach(function(currentBtn){
   currentBtn.addEventListener('click', () => {
-      let click = document.querySelector('.click');
       click.play();
   })
 })
 
-
+//sound for the equal button 
 const equalBtn = document.querySelector('.equalBtn');
 equalBtn.addEventListener('click', () => { 
     let equalClick = document.querySelector('.equalClick');
@@ -16,53 +25,78 @@ equalBtn.addEventListener('click', () => {
 
 
 
-
-
-
-
-
-
-
 function add(num1,num2) {
-    return num1 + num2; 
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    return num1 + num2;
     
 }
 
 function subtract(num1,num2) {
-    return num1 - num2; 
+    return Number(num1) - Number(num2);
     
 }
 
 function multiply(num1,num2) {
-    return num1 * num2; 
+    return Number(num1) * Number(num2);
     
 }
 
 function divide(num1,num2) {
-    return num1 / num2; 
+    return Number(num1) / Number(num2);
     
 }
 
 
-function operate (operator, num1, num2) { 
+function operate (num1, operator, num2) { 
+
+num1 = screen.textContent[0];
+operator = screen.textContent[1];
+num2 = screen.textContent[2];
+
     if (operator == '+') {
-        return add(num1,num2);
+        screen.textContent = add(num1,num2);
     }
     else if (operator == '-') {
         return subtract(num1,num2);
     }
-    else if (operator == 'multiply') {
+    else if (operator == '*') {
         return multiply(num1,num2);
     }
-    else if (operator == 'divide') {
+    else if (operator == '/') {
         return divide(num1,num2); 
     }
     else { 
         alert('g')
     }
+
+
+    
 }
 
 
 
+function clearDisplay () { 
+    
+    screen.textContent = "";
+
+}
+function deleteDisplay () {
+    newStr = screen.textContent.slice(0,-1);
+    screen.textContent = newStr;
+
+}
+
+function display(num){
+screen.textContent += num;
+console.log(screen.textContent);
+}
+
+// function calculate() { 
+// let equation = screen.textContent;
+// let eqationAnswer = eval(equation);
+// screen.textContent = eqationAnswer;
+// }
 
 
