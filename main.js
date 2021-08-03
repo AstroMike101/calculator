e344027d993b52edd284c21cf825e1729c4797df
Michael <AstroMike101@gmail.com>
2021-08-03 02:04:13 -0700

    all operators functional

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ let deleteButton = document.querySelector(".deleteButton");
 let clearButton = document.querySelector(".clearButton");
 let click = document.querySelector(".click"); // sound of click on buttons
 let equation = 0;
+let operators =  ['+', '-', '*', '/', '='];
+
+
+
+
+
+
 
 //plays a sound whenever a button is clicked
 const buttons = document.querySelectorAll(".btn");
@@ -24,10 +31,38 @@ function getBeforePlus(str) {
   return(str.substring(0, str.indexOf("+"))); 
 }
 
-
-
 function getAfterPlus(str) { 
   return(str.split('+')[1]);
+}
+
+
+
+function getBeforeMinus(str) { 
+  return(str.substring(0, str.indexOf("-"))); 
+}
+
+function getAfterMinus(str) { 
+  return(str.split('-')[1]);
+}
+
+
+
+
+function getBeforeTimes(str) { 
+  return(str.substring(0, str.indexOf("*"))); 
+}
+
+function getAfterTimes(str) { 
+  return(str.split('*')[1]);
+}
+
+
+function getBeforeDivide(str) { 
+  return(str.substring(0, str.indexOf("/"))); 
+}
+
+function getAfterDivide(str) { 
+  return(str.split('/')[1]);
 }
 
 
@@ -40,37 +75,74 @@ function add(num1, num2) {
 
 
 function subtract(num1, num2) {
-  return Number(num1) - Number(num2);
+  num1 = Number(num1);
+  num2 = Number(num2);
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-  return Number(num1) * Number(num2);
+  num1 = Number(num1);
+  num2 = Number(num2);
+  return num1*num2;
 }
 
 function divide(num1, num2) {
-  return Number(num1) / Number(num2);
+  num1 = Number(num1);
+  num2 = Number(num2);
+  return num1/num2;
 }
 
 
 
 function operate(num1, operator, num2) {
   
-  operatorLocation = screen.textContent.indexOf("+");
-  num1 = getBeforePlus(screen.textContent);
-  operator = screen.textContent[operatorLocation];
-  num2 = getAfterPlus(screen.textContent);
+  if (screen.textContent.includes("+")) { 
+    operatorLocation = screen.textContent.indexOf("+");
+    num1 = getBeforePlus(screen.textContent);
+    operator = screen.textContent[operatorLocation];
+    num2 = getAfterPlus(screen.textContent);
+  }
+  else if (screen.textContent.includes("-")) {
+    operatorLocation = screen.textContent.indexOf("-");
+    num1 = getBeforeMinus(screen.textContent);
+    operator = screen.textContent[operatorLocation];
+    num2 = getAfterMinus(screen.textContent);
+  }
+
+  else if (screen.textContent.includes("*")) {
+    operatorLocation = screen.textContent.indexOf("*");
+    num1 = getBeforeTimes(screen.textContent);
+    operator = screen.textContent[operatorLocation];
+    num2 = getAfterTimes(screen.textContent);
+  }
+  else if (screen.textContent.includes("/")) {
+    operatorLocation = screen.textContent.indexOf("/");
+    num1 = getBeforeDivide(screen.textContent);
+    operator = screen.textContent[operatorLocation];
+    num2 = getAfterDivide(screen.textContent);
+  }
+
+  else { 
+    alert("bruh do an actual equation");
+  }
+
+
+
+
+  // operatorLocation = screen.textContent.indexOf("+");
+  // num1 = getBeforePlus(screen.textContent);
+  // operator = screen.textContent[operatorLocation];
+  // num2 = getAfterPlus(screen.textContent);
 
   if (operator == "+") {
     screen.textContent = add(num1, num2);
   } else if (operator == "-") {
-    return subtract(num1, num2);
+    screen.textContent = subtract(num1, num2);
   } else if (operator == "*") {
-    return multiply(num1, num2);
+    screen.textContent = multiply(num1, num2);
   } else if (operator == "/") {
-    return divide(num1, num2);
-  } else {
-    alert("g");
-  }
+    screen.textContent = divide(num1, num2);
+  } 
 }
 
 function clearDisplay() {
